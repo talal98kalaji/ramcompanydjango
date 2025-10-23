6717fdefb35283deeaa537d3d0e058f2271d0392
unknown <talal.kalaji.alep@gmail.com>
2025-10-23 17:53:28 +0300

    Many updates

--- ramcompany/settings.py
+++ ramcompany/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['*']
+DEBUG = False
+ALLOWED_HOSTS = ['talal98kalaji.pythonanywhere.com']
 
 # Application definition
 
